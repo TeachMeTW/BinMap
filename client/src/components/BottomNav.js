1fe8d5587baa4df03ebe071d977d4c86422fbc9b
TeachMeTW <teachmetheways652@gmail.com>
2024-01-02 17:04:22 -0800

    Add Bins proto

--- client/src/components/BottomNav.js
+++ client/src/components/BottomNav.js
@@ -5,13 +5,27 @@ import {
   Box,
   Paper,
 } from "@mui/material";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import ClusterMap from "./map/ClusterMap";
+import Bin from "./bins/Bin";
+import AddBin from "./addBin/AddBin";
 
 const BottomNav = () => {
   const [value, setValue] = useState(0);
-
+  const ref = useRef();
+  useEffect(() => {
+    ref.current.ownerDocument.body.scrollTop = 0;
+  }, [value]);
   return (
-    <Box>
+    <Box ref={ref}>
+      {
+        {
+          0: <ClusterMap />,
+          1: <Bin />,
+          2: <AddBin />,
+        }[value]
+      }
+
       <Paper
         elevation={3}
         sx={{ position: "fixed", bottom: 0, left: 0, right: 0, zIndex: 2 }}
--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,63 +1,75 @@
-import React from 'react'
-import {AppBar, Container, Toolbar, Box, IconButton, Typography, Button} from '@mui/material'
-import {Menu} from '@mui/icons-material'
+import React from "react";
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  Box,
+  IconButton,
+  Typography,
+  Button,
+} from "@mui/material";
+import { Menu } from "@mui/icons-material";
 
-import { useValue } from '../context/ContextProvider'
-import UserIcons from './user/UserIcons'
-
-
+import { useValue } from "../context/ContextProvider";
+import UserIcons from "./user/UserIcons";
 
 const NavBar = () => {
-
-    const {state:{currentUser}, dispatch} = useValue()
-
+  const {
+    state: { currentUser },
+    dispatch,
+  } = useValue();
 
   return (
-
-    /* AppBar from Material UI */
-    <AppBar>
+    <>
+      <AppBar>
         {/* Containter from Material UI stores the individual Nav Components*/}
-        <Container maxWidth='lg'>
-            {/* DisableGutters removes padding */}
-            <Toolbar disableGutters>
-                {/* Box with margin from right */}
-                <Box sx={{mr:1}}>
-                    {/* Menu Icon */}
-                    <IconButton size='large' color='inherit'>
-                        <Menu />
-                    </IconButton>
-                </Box>
-                {/* Typography Logo/Text, Can be turned into an Icon*/}
-                <Typography
-                variant='h6'
-                component='h1'
-                noWrap
-                sx={{flexGrow:1, display:{xs:'none', md:'flex'}}}
-                >
-                    BinMap
-                </Typography>
+        <Container maxWidth="lg">
+          {/* DisableGutters removes padding */}
+          <Toolbar disableGutters>
+            {/* Box with margin from right */}
+            <Box sx={{ mr: 1 }}>
+              {/* Menu Icon */}
+              <IconButton size="large" color="inherit">
+                <Menu />
+              </IconButton>
+            </Box>
+            {/* Typography Logo/Text, Can be turned into an Icon*/}
+            <Typography
+              variant="h6"
+              component="h1"
+              noWrap
+              sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}
+            >
+              BinMap
+            </Typography>
 
-                {/* For smaller screens*/}
-                <Typography
-                variant='h6'
-                component='h1'
-                noWrap
-                sx={{flexGrow:1, display:{xs:'flex', md:'none'}}}
-                >
-                    BM
-                </Typography>
-                {!currentUser ? (
-                    <Button color='inherit' variant='outlined' onClick={() => dispatch({type: 'OPEN_LOGIN'})}>Login</Button>
-                ) : (
-                    <UserIcons/>
-
-                )}
-                {/* Login Button */}
-            </Toolbar>
+            {/* For smaller screens*/}
+            <Typography
+              variant="h6"
+              component="h1"
+              noWrap
+              sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+            >
+              BM
+            </Typography>
+            {!currentUser ? (
+              <Button
+                color="inherit"
+                variant="outlined"
+                onClick={() => dispatch({ type: "OPEN_LOGIN" })}
+              >
+                Login
+              </Button>
+            ) : (
+              <UserIcons />
+            )}
+            {/* Login Button */}
+          </Toolbar>
         </Container>
+      </AppBar>
+      <Toolbar />
+    </>
+  );
+};
 
-    </AppBar>
-  )
-}
-
-export default NavBar
+export default NavBar;
